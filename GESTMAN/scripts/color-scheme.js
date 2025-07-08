ed0b4cf6a4435d6db922a00c84cffbe42d2d0abe
ReichertDouglas <reichert_douglas@hotmail.com>
2025-07-08 12:26:16 -0300

    alteração repositório

--- GESTMAN/scripts/color-scheme.js
+++
@@ -1,4 +0,0 @@
-const html = document.documentElement;
-        const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-        html.setAttribute('data-bs-theme', prefersDark ? 'dark' : 'light');
